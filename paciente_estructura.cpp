5418a322d57f47242a6727c956fe66bad12589c1
Nicoo628 <nico.moralesr2000@gmail.com>
2024-08-27 10:00:12 -0400

    Se agrego el peso a los pacientes y modificaciones esteticas

--- paciente_estructura.cpp
+++ paciente_estructura.cpp
@@ -2,11 +2,12 @@
 #include <string>
 
 using namespace std ;
-
+//STRUCT
 struct Paciente {
     string nombre;
     int edad;
     double altura;
+    double peso;
 };
 
 int main() {
@@ -17,24 +18,30 @@ int main() {
     paciente3.nombre = "Antonio";
     paciente3.edad = 20;
     paciente3.altura = 1.85;
+    paciente3.peso = 100;
 
     paciente2.nombre = "Cristopher";
     paciente2.edad = 20;
     paciente2.altura = 1.80;
+    paciente2.peso = 70;
 
     paciente1.nombre = "Diego";
     paciente1.edad = 20;
     paciente1.altura = 1.63;
-//Paciente 3
+    paciente1.peso = 89;
+//Paciente 3 ATRIBUTOS
     cout << "Nombre Paciente 3: " << paciente3.nombre << "\n";
     cout << "Edad Paciente 3: " << paciente3.edad << "\n";
-    cout << "Altura Paciente 3: " << paciente3.altura << "\n";
+    cout << "Altura Paciente 3: " << paciente3.altura << "cm\n";
+    cout << "Peso Paciente 3:" << paciente3.peso << "kg\n";
 //Paciente 2
     cout << "Nombre Paciente 2: " << paciente2.nombre << "\n";
     cout << "Edad Paciente 2: " << paciente2.edad << "\n";
-    cout << "Altura Paciente 2: " << paciente2.altura << "\n";
+    cout << "Altura Paciente 2: " << paciente2.altura << "cm\n";
+    cout << "Peso Paciente 1:" << paciente3.peso << "kg\n";
 //Paciente 1
     cout << "Nombre Paciente 1: " << paciente1.nombre << "\n";
     cout << "Edad Paciente 1: " << paciente1.edad << "\n";
-    cout << "Altura Paciente 1: " << paciente1.altura << "\n";
+    cout << "Altura Paciente 1: " << paciente1.altura << "cm\n";
+    cout << "Peso Paciente 1:" << paciente3.peso << "kg\n";
 }
