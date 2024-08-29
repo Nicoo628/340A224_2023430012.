c5e6163cd5545dbf9a0b4ddd754708350015aa62
Nicoo628 <nico.moralesr2000@gmail.com>
2024-08-29 00:36:13 -0400

    Se realizo el archivo con una lista en la que estan las funciones para añadir, eliminar y imprimir a los pacientes

---
+++ paciente_dlist.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+using namespace std ;
+// Estructura para una lista
+struct Paciente {
+    string nombre;
+    int edad;
+    double altura;
+    double peso;
+    Paciente* next;
+};
+
+// Funcion para añadir un nuevo paciente a la lista
+
+void AñadirPaciente(Paciente*& head, const string& nombre, int edad, double peso,double altura) {
+    Paciente* NuevoPaciente = new Paciente();
+    NuevoPaciente->nombre = nombre;
+    NuevoPaciente->edad = edad;
+    NuevoPaciente->altura = altura;
+    NuevoPaciente->peso = peso;
+    NuevoPaciente->next = head;
+    head = NuevoPaciente;
+}
+
+//Funcion para eliminar una persona de la lista por el nombre
+void EliminarPaciente(Paciente*& head, const string& nombre) {
+    Paciente* actual = head;
+    Paciente* anterior = nullptr;
+
+    while (actual != nullptr && actual->nombre !=nombre) {
+        anterior = actual;
+        actual = actual->next;
+    }
+
+    if (actual != nullptr) {
+        if (anterior == nullptr) {
+            head = actual->next;
+        } else {
+            anterior->next = anterior->next;
+        }
+        delete actual;
+    }
+}
+
+// Funcion para imprimir toda la gente de la lista
+void ImprimirPaciente(const Paciente* head) {
+    const Paciente* actual = head;
+    while (actual != nullptr) {
+        cout << "Nombre: " << actual -> nombre << "\n";
+        cout << "Edad: " << actual -> edad << "\n";
+        cout << "Peso: " << actual -> peso << "kg\n";
+        cout << "altura: "<< actual -> altura << "cm\n";
+
+
+        actual = actual->next;
+
+    }
+}
+
+int main() {
+    Paciente* head= nullptr;
+    //orden: nombre, edad, peso, altura
+AñadirPaciente(head, "Cristopher Reyes", 21, 90, 1.83);
+AñadirPaciente(head, "Nicolas Morales", 19,70, 1.72);
+AñadirPaciente(head, "Eustaquio Tamarindo", 94, 70, 2.01);
+
+ImprimirPaciente(head);
+EliminarPaciente(head, "a");// no me dejaba poner sin el "a" porque no se llamaban todos los parametros de la funcion
+return 0;
+}
