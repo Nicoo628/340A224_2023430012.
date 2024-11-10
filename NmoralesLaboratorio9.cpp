05a9a16df490d90b2ec0087f0fe4b627496f10af
Nicoo628 <nico.moralesr2000@gmail.com>
2024-11-10 16:44:54 -0300

    Se fueron añadiendo diversas funciones y avanzando con las prueba lineal, cuadratica,doble direccion,despues de cada insersion se imprime el estado,

--- NmoralesLaboratorio9.cpp
+++ NmoralesLaboratorio9.cpp
@@ -28,10 +28,60 @@ void imprimirTabla(const int tabla[]) {
     }
     cout << endl;
 }
+// Tabla hash con encadenamiento
+void imprimirTablaEncadenamiento(const list<int> tabla[]) {
+    cout << "Estado actual de la tabla hash con encadenamiento:" << endl;
+    for (int i = 0; i < TAMANIO_TABLA; i++) {
+        cout << i << ": ";
+        for (int valor : tabla[i]) {
+            cout << valor << " -> ";
+        }
+        cout << "NULL" << endl;
+    }
+    cout << endl;
+}
+
+// Prueba Lineal
+void pruebaLineal(int tabla[], int clave) {
+    int indice = funcionHash(clave);
+    int indiceInicial = indice;
+    int desplazamiento = 0;
+
+    while (tabla[indice] != VACIO) {
+        indice = (indice + 1) % TAMANIO_TABLA;
+        desplazamiento++;
+    }
+    tabla[indice] = clave;
+    cout << "Clave " << clave << " insertada en posicion " << indice 
+         << " con desplazamiento de " << desplazamiento 
+         << " desde la posicion inicial " << indiceInicial << "." << endl;
+}
+
+// Prueba Cuadratica
+void pruebaCuadratica(int tabla[], int clave) {
+    int indice = funcionHash(clave);
+    int desplazamiento = 0;
+    int i = 1;
+
+    while (tabla[indice] != VACIO) {
+        indice = (funcionHash(clave) + i * i) % TAMANIO_TABLA;
+        desplazamiento++;
+        i++;
+    }
+    tabla[indice] = clave;
+    cout << "Clave " << clave << " insertada en posicion " << indice 
+         << " con desplazamiento cuadratico de " << desplazamiento << "." << endl;
+}
 
 int main() {
     int tabla[TAMANIO_TABLA] = {VACIO}; // Inicialización de la tabla hash
+    pruebaLineal(tabla, 25);
+    imprimirTabla(tabla);
+
+    pruebaCuadratica(tabla, 15);
     imprimirTabla(tabla);
 
     return 0;
 }
+
+
